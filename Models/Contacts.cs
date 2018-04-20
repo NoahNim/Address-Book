0792e182438ae31db97b9270aa94b20b7aad9adb
Noah Medoff <noahmedoff@Noahs-MacBook-Pro.local>
2018-04-20 13:21:14 -0700

    create contacts model

---
+++ Models/Contacts.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using System;
+
+namespace Contacts.Models
+{
+  public class Contact
+  {
+    private string _name;
+    private string _phone;
+    private string _address
+    private int _id;
+    private static List<Contact> _instances = new List<Contact>{};
+
+    public Contact (string name, string phone, string address)
+    {
+      _name = name;
+      _phone = phone;
+      _adress = address;
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+        public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Contact Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+}
